4fd06eac4e0147bcf21978d23f24bb6278269745
PALIGON-dev <deloriansandstorm@gmail.com>
2024-11-26 20:30:07 +0300

    небольшое редактирование, добавление файла json для теста

--- code.py
+++ code.py
@@ -7,22 +7,12 @@ class Config:
         self.json_data = json_data
         self.constants = {}
 
-    def translate(self):
-        return self._process_dict(self.json_data)
-
-    def _process_value(self, value):
-        if isinstance(value, str):
-            if re.match(r'^\$\(.*\)$', value):
-                constant_name = value[2:-1]
-                if constant_name not in self.constants:
-                    raise ValueError(f"Undefined constant: {constant_name}")
-                return str(self.constants[constant_name])
-            return f'"{value}"'
-        return str(value)
-
     def _process_dict(self, data, indent=0):
         if not isinstance(data, dict):
-            raise ValueError("Element is wrong")
+            raise ValueError("el is not wright")
+
+    def translate(self):
+        return self._process_dict(self.json_data)
 
         result = []
         for key, value in data.items():
@@ -36,8 +26,18 @@ class Config:
                 result.append(self._process_dict(value, indent + 2))
                 result.append(f"{' ' * indent}}}")
             else:
-                raise ValueError(f"Unsupported value type {key}: {type(value)}")
+                raise ValueError(f"Unsupported type {key}: {type(value)}")
         return "\n".join(result)
+
+    def _process_value(self, value):
+        if isinstance(value, str):
+            if re.match(r'^\$\(.*\)$', value):
+                constant_name = value[2:-1]
+                if constant_name not in self.constants:
+                    raise ValueError(f"Unrecognised : {constant_name}")
+                return str(self.constants[constant_name])
+            return f'"{value}"'
+        return str(value)
 
 def main():
     if len(sys.argv) < 2:
@@ -50,7 +50,7 @@ def main():
         with open(input_file, 'r', encoding='utf-8') as file:
             json_data = json.load(file)
     except Exception as e:
-        print(f"Error reading JSON file: {e}")
+        print(f"Json is not opened: {e}")
         sys.exit(1)
 
     try:
@@ -58,7 +58,7 @@ def main():
         result = translator.translate()
         print(result)
     except Exception as e:
-        print(f"Translation error: {e}")
+        print(f"Translation is wrong: {e}")
         sys.exit(1)
 
 if __name__ == "__main__":
